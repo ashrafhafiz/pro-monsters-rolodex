ba379b0308f25d3cb883715c19e67215a052deef
Ashraf Hafiz <Ashraf.Hafiz@yahoo.com>
2020-04-03 14:07:28 +0200

    Updates

---
+++ precache-manifest.a1ee2a79b16c79dff0d94e02ee45b76a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7c8f6c8acaa978a3512af2e032d2ec",
+    "url": "/pro-monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "e0500c736d2676636b76",
+    "url": "/pro-monsters-Rolodex/static/css/main.a91b2e8b.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/pro-monsters-Rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pro-monsters-Rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0500c736d2676636b76",
+    "url": "/pro-monsters-Rolodex/static/js/main.3f7cce6b.chunk.js"
+  },
+  {
+    "revision": "daef15253a15eababbcf",
+    "url": "/pro-monsters-Rolodex/static/js/runtime-main.a88f85ed.js"
+  }
+]);
